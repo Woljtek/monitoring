9f27ea11238c1b694fd0e5af7e3328b8a9571738
gdurand-cs <90031258+gdurand-cs@users.noreply.github.com>
2022-12-01 15:33:34 +0100

    Update FilterGroup.java

--- trace-processor/applications/processor/trace-filter/src/main/java/eu/csgroup/coprs/monitoring/tracefilter/rule/FilterGroup.java
+++ trace-processor/applications/processor/trace-filter/src/main/java/eu/csgroup/coprs/monitoring/tracefilter/rule/FilterGroup.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package eu.csgroup.coprs.monitoring.tracefilter.rule;
 
 import eu.csgroup.coprs.monitoring.common.bean.BeanAccessor;
@@ -38,43 +37,3 @@ public class FilterGroup implements Function<BeanAccessor, Optional<Filter>> {
                 .findFirst();
     }
 }
-||||||| b8aeece
-=======
-package eu.csgroup.coprs.monitoring.tracefilter.rule;
-
-import eu.csgroup.coprs.monitoring.common.bean.BeanAccessor;
-import eu.csgroup.coprs.monitoring.common.properties.ReloadableYamlPropertySourceFactory;
-
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.beans.BeanWrapper;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.Predicate;
-
-@Data
-@Configuration
-@PropertySource(name = "filterGroup", value = "${filter.path}", factory = ReloadableYamlPropertySourceFactory.class)
-@ConfigurationProperties()
-@Slf4j
-public class FilterGroup implements Function<BeanAccessor, Optional<Filter>> {
-    List<Filter> filters;
-
-    @Override
-    public Optional<Filter> apply(BeanAccessor node) {
-        final Predicate<Filter> checkFilter = filter -> filter.test(node);
-
-        return filters
-                .stream()
-                .peek(filter -> log.trace("Apply filter %s".formatted(filter.getName())))
-                .filter(checkFilter)
-                .findFirst();
-    }
-}
->>>>>>> dev
